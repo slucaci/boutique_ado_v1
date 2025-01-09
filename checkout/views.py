6e4d278a813ab816dbe1d6a2e509b750a35d46b6
slucaci <serjlucaci@gmail.com>
2025-01-09 18:32:58 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QfQWgKlTYMwRyTyns3xzlqBA4zgchO6RVqvK0idvzNmIIsp4mZHAIj3JAZuozlVR7nnWUXKKt6NMqsDyY4em3Mg00Uw9jREYk',
+        'client_secret' : 'test client secret',
     }
 
     return render(request, template, context)
